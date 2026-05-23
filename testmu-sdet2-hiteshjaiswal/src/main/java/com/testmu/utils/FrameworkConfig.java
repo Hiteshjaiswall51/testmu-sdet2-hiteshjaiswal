93b5a956e656618509aaf1e765b408c00194efaf
hitesh jaiswal <hitesh.jaiswal@opstree.com>
2026-05-23 11:57:51 +0530

    project properties and config framework setup done

---
+++ testmu-sdet2-hiteshjaiswal/src/main/java/com/testmu/utils/FrameworkConfig.java
@@ -0,0 +1,29 @@
+package com.testmu.utils;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+
+@Config.Sources({
+        "System:properties",
+        "System:env",
+        "classpath:project.properties"
+})
+
+public interface FrameworkConfig extends Config {
+
+    @DefaultValue("dev")
+    String environment();
+
+    @Key("${environment}.url")
+    String url();
+
+    @Key("${environment}.uri")
+    String uri();
+
+    @Key("${environment}.username")
+    String username();
+
+    @Key("${environment}.password")
+    String password();
+}
